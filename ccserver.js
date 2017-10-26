29eaf3655afa54ae54d74edb0f9b8a122df992bd
PippoGit <filipposcotto@imac.local>
2017-10-26 20:30:29 +0200

    nodejs remote mediacontrol macos

---
+++ ccserver.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var path = require('path');
+var osa = require('node-osascript');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Remote Control Center started on port 3000.');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join('index.html'));
+});
+
+app.get('/volumeup', function (req, res) {
+  changeVolume(1);
+  res.sendStatus(200);
+});
+
+app.get('/volumedown', function (req, res) {
+  changeVolume(-1);
+  res.sendStatus(200);
+});
+
+app.get('/next', function (req, res) {
+  iTunes("next track");
+  res.sendStatus(200);
+});
+
+app.get('/previous', function (req, res) {
+  iTunes("previous track");
+  res.sendStatus(200);
+});
+
+app.get('/playpause', function (req, res) {
+  iTunes("playpause");
+  res.sendStatus(200);
+});
+
+app.get('/itunes', function (req, res) {
+  var c = req.query.cmd;
+  iTunes(c);
+  res.sendStatus(200);
+});
+
+function changeVolume(op) {
+  osa.execute("output volume of (get volume settings)", function(err, result, raw) {
+    if (err) return console.error(err);
+
+    var volume = (result + (op*5) >= 100)? 100:result+(op*5);
+    osa.execute("set volume output volume " + volume);
+  });
+}
+
+function iTunes(cmd)
+{
+  osa.execute("if application \"iTunes\" is running then return \"1\"", function (err, result, raw) {
+    if (err) return console.error(err);
+
+    if(result)
+      osa.execute("tell application \"iTunes\" to " + cmd);
+    else
+      mediaFunction(cmd);
+  });
+}
+
+function mediaFunction(cmd)
+{
+  switch (cmd) {
+    case 'playpause':
+      osa.execute("tell application \"System Events\" to keystroke space");
+      break;
+    case 'next track':
+      osa.execute("tell application \"System Events\" to key code 124");
+      break;
+    case 'previous track':
+    osa.execute("tell application \"System Events\" to key code 123");
+      break;
+
+    default:
+      console.log("error");
+  }
+}
---
+++ public/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+  var timeout = 0;
+
+  $("#play").on('click tap', playPause);
+  $("#next").on('click tap', iTunesNextTrack);
+  $("#before").on('click tap', iTunesPreviousTrack);
+  $("#volumeup").on('touchstart mousedown click tap', function(e){
+      increaseVolume();
+      if(e.type == "click")
+        return;
+      timeout = setInterval(function(){
+          increaseVolume();
+      }, 500);
+      return false;
+  });
+  $("#volumedown").on('touchstart mousedown click tap', function(e){
+      decreaseVolume();
+      if(e.type == "click")
+        return;
+      timeout = setInterval(function(){
+          decreaseVolume();
+      }, 500);
+      return false;
+  });
+
+  $("#volumeup, #volumedown").on('touchend mouseup touchcancel', function(){
+      clearInterval(timeout);
+      return false;
+  });
+});
+
+function playPause()
+{
+  $.get("playpause");
+}
+
+function increaseVolume()
+{
+  $.get("volumeup");
+}
+
+function decreaseVolume()
+{
+  $.get("volumedown");
+}
+
+function iTunesNextTrack()
+{
+  $.get("next");
+}
+
+function iTunesPreviousTrack()
+{
+  $.get("previous");
+}
